060cd010ea387a46fae7a01a2f22ae973186eede
crayon409 <wangqj409@gmail.com>
2025-02-16 01:19:46 +0800

    init

--- cmd/gotest1/main.go
+++ cmd/gotest1/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"net/http"
+)
 func main() {
 	println("hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+	http.ListenAndServe(":8000", mux)
 }
